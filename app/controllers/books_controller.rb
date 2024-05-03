2d72b1239f12304254e5dac601b0c80d035ce095
YuIshikawa <166091720+29yu03@users.noreply.github.com>
2024-05-03 13:44:07 +0000

    rei

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,27 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
   end
 
+def create
+  book = Book.new(book_params)
+  book.save
+  redirect_to '/books/show'
+end
+
+
   def index
+    @books =  Book.all
   end
 
   def show
+    #@books =  Book.all
   end
 
   def edit
+  end
+
+private
+  def book_params
   end
 end
